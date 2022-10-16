0c911be87e7dd0af89859d6a69b3dc402a6f44b4
Harry Kim <harryk9395@gmail.com>
2022-10-15 18:23:16 -0700

    Somewhat Add test calc

--- src/GradeBook.java
+++ src/GradeBook.java
@@ -2,14 +2,22 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class GradeBook {
+    private static double testCalc(double score, int t, double weight) {
+        double testGrade = (score*weight)/t;
+        return testGrade;
+    }
+
     public static void main(String[] args) {
         //Hard-coded variables | Tests: 70%, Homework: 20%, Participation: 10%
         String name = "Jamal";
-        float testWeight = 0.70f;
-        float homeworkWeight = 0.20f;
-        float participationWeight = 0.10f;
-        String again = "Yes";
+        double testWeight = 0.70f;
+        double homeworkWeight = 0.20f;
+        double participationWeight = 0.10;
+        int tterm = 1;
+        int hterm = 1;
+        int pterm = 1;
 
+        String again = "Yes";
         while(!Objects.equals(again,"Stop"))
         {
             //Assignment user input
@@ -18,12 +26,18 @@ public class GradeBook {
             String assignment = assignmentName.nextLine();
             Scanner assignmentScore = new Scanner(System.in);
             System.out.print("Enter score: ");
-            float score = Float.parseFloat(assignmentName.nextLine());
+            double score = Double.parseDouble(assignmentName.nextLine());
             Scanner assignmentCategory = new Scanner(System.in);
-            System.out.print("Enter category: ");
+            System.out.print("Enter category (Test, Homework, Participation: ");
             String category = assignmentName.nextLine();
 
-            //Weight method
+            if (category.equals("Test"))
+            {
+                System.out.println(testCalc(score, tterm, testWeight));
+                tterm++;
+            }
+
+
             Scanner endLoop = new Scanner(System.in);
             System.out.print("Enter any key to continue, enter 'Stop' to stop inputting scores: ");
             again = assignmentName.nextLine();
